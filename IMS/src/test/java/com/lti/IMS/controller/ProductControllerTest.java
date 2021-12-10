a59b5b4f561902b92753032466a15d451cde0551
Administrator <Administrator@host.docker.internal>
2021-12-10 22:08:49 +0530

    ADD

--- IMS/src/test/java/com/lti/IMS/controller/ProductControllerTest.java
+++ IMS/src/test/java/com/lti/IMS/controller/ProductControllerTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Date;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
